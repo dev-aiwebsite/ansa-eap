bec8712c8fcc03b4576b3c4bf012cac531a6cd87
aidev <dev@aiwebsiteservices.com>
2025-09-24 14:58:14 +0800

    added bottom padding on mobile for scrolling

--- src/app/(authenticated)/learning-development/layout.tsx
+++ src/app/(authenticated)/learning-development/layout.tsx
@@ -1,13 +1,16 @@
 import LayoutWithBreadcrumb, {
   LayoutWithChildrenProps,
 } from "@/components/layouts/layoutWIthBreadCrumb";
+import Container from "@/components/ui/container";
 import { UserActivityProvider } from "@/context/userActivitiesContext";
 
 const Layout = ({ children }: LayoutWithChildrenProps) => {
   return (
+    <Container>
     <UserActivityProvider>
       <LayoutWithBreadcrumb>{children}</LayoutWithBreadcrumb>
     </UserActivityProvider>
+    </Container>
   );
 };
 
