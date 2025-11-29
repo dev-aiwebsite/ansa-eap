29656f6e63ba8b08bf146cca762c85025bd17bab
aidev <dev@aiwebsiteservices.com>
2025-11-29 13:27:33 +0800

    mobile nav - convert resources icon to a popup menu containing ansa resources, partners, marli

--- src/components/ui/nav/NavMobile.tsx
+++ src/components/ui/nav/NavMobile.tsx
@@ -5,10 +5,12 @@ import {
   IconGrid,
   IconServices,
 } from "@/icons";
+import { cn } from "@/lib/utils";
 import { UserCog } from "lucide-react";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "../button";
-import { cn } from "@/lib/utils";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../dropdown-menu";
 import { UserMenu } from "../userMenu";
 
 const items = [
@@ -22,17 +24,17 @@ const items = [
     textClassName: "",
     link: "/critical-response",
   },
-  { icon: IconDocuments, label: "Resources", link: "/resources", iconClassName: 'stroke-[3.5px]'},
+  { icon: IconDocuments, label: "Resources", link: "/resources", iconClassName: 'stroke-[3.5px]' },
   { icon: UserCog, label: "Account", link: "/settings/account" },
 ];
 const NavMobile = () => {
   const path = usePathname();
-  
+
   return (
     <div className="mobile-nav">
       <div className="grid grid-cols-5 gap-2">
         {items.map(({ icon: Icon, label, className, textClassName, link, iconClassName }) => {
-          const activeClass = path == link &&  "text-primary [&_>_svg]:stroke-primary";
+          const activeClass = path == link && "text-primary [&_>_svg]:stroke-primary";
           if (label == "Account") {
             return (
               <UserMenu
@@ -45,6 +47,49 @@ const NavMobile = () => {
               />
             );
           }
+          if (label == 'Resources') {
+
+            return <DropdownMenu key={label}>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  href={link}
+                  key={label}
+                  className={cn(
+                    "!p-2 text-[#888] font-[600] rounded-xl text-base flex-col h-auto",
+                    className,
+                    activeClass
+                  )}
+                  variant="ghost"
+                >
+
+                  <Icon strokeWidth={1.2} className={cn("!w-[24px] !h-[24px] block", iconClassName)} />
+                  <span className={cn("text-[9px]", textClassName)}>{label}</span>
+                </Button>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent align="end" className="z-[9999999]">
+                <DropdownMenuItem asChild>
+                  <Link href="/resources" className="flex items-center gap-2">
+                    ANSA Resources
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <Link href="/short-courses/partners" className="flex items-center gap-2">
+                    Partners
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <Link href="/short-courses/marli" className="flex items-center gap-2">
+                    Marli
+                  </Link>
+                </DropdownMenuItem>
+
+              </DropdownMenuContent>
+
+            </DropdownMenu>
+
+          }
+
           return (
             <Button
               href={link}
@@ -56,7 +101,7 @@ const NavMobile = () => {
               )}
               variant="ghost"
             >
-              
+
               <Icon strokeWidth={1.2} className={cn("!w-[24px] !h-[24px] block", iconClassName)} />
               <span className={cn("text-[9px]", textClassName)}>{label}</span>
             </Button>
